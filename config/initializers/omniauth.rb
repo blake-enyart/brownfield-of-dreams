72b8c38c4596f91606e25f925888085178abe44b
Blake <blake.enyart@gmail.com>
2019-05-11 13:37:11 -0600

    incude scope on OmniAuth request

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET']
-  {  :scope => ['repo','user']  }
-  #SCOPE LATER
+  provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'], scope: "user,repo,gist"
 end
