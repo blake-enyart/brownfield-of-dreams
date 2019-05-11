455c582131c301bc38869bc485a3069b5e6eb14f
Blake <blake.enyart@gmail.com>
2019-05-11 11:20:25 -0600

    Built link and channel to create token in GithubCredentials

---
+++ app/controllers/github_credentials_controller.rb
@@ -0,0 +1,12 @@
+class GithubCredentialsController < ApplicationController
+  def create
+    user_info = request.env['omniauth.auth']
+    token = user_info['credentials']['token']
+    if !current_user.github_credential
+      current_user.github_credential = GithubCredential.create(token: token)
+    else
+      current_user.github_credential.update(token: token)
+    end
+    redirect_to '/dashboard'
+  end
+end
