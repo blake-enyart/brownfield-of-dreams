c08e475e12c3ee773c3b688e4ca7ba93c2379665
Blake Enyart <38588295+blake-enyart@users.noreply.github.com>
2019-05-11 14:35:18 -0600

    Update repo_spec.rb

--- spec/models/repo_spec.rb
+++ spec/models/repo_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 describe Repo do
-  it "has attributes" do
+  it 'has attributes' do
     attributes = {
-           name: "Michael Jackson",
-           html_url: '5'
-       }
+     name: 'Michael Jackson',
+     html_url: '5'
+     }
 
     member = Repo.new(attributes)
-    expect(member.name).to eq("Michael Jackson")
-    expect(member.html_url).to eq("5")
+    expect(member.name).to eq('Michael Jackson')
+    expect(member.html_url).to eq('5')
   end
 end
