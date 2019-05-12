bea804135b31220b24ba9dd4142c92172eddd363
Blake Enyart <38588295+blake-enyart@users.noreply.github.com>
2019-05-11 19:25:18 -0600

    Update search_results_facade.rb

--- app/facades/search_results_facade.rb
+++ app/facades/search_results_facade.rb
@@ -6,13 +6,13 @@ class SearchResultsFacade
   end
 
   def repositories
-    raw_repo_data = get_json('repos').take(5).map do |raw_repo|
+    get_json('repos').take(5).map do |raw_repo|
       DataParse::Repo.new(raw_repo)
     end
   end
 
   def followers
-    raw_repo_data = get_json('followers').map do |raw_user|
+    get_json('followers').map do |raw_user|
       DataParse::GithubUser.new(raw_user)
     end
   end
