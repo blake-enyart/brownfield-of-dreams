0d96e3a2bb00fa54075a39d6698b0a46e52aea33
Blake <blake.enyart@gmail.com>
2019-05-11 19:15:58 -0600

    Add GithubUser test in data_parse

---
+++ spec/models/data_parse/github_user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe DataParse::GithubUser do
+  it 'has attributes' do
+    attributes = {
+     login: 'Michael Jackson',
+     html_url: '5'
+     }
+
+    member = DataParse::GithubUser.new(attributes)
+    expect(member.handle).to eq('Michael Jackson')
+    expect(member.github_url).to eq('5')
+  end
+end
--- spec/models/data_parse/repo_spec.rb
+++ spec/models/data_parse/repo_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-describe Repo do
+describe DataParse::Repo do
   it 'has attributes' do
     attributes = {
      name: 'Michael Jackson',
      html_url: '5'
      }
 
-    member = Repo.new(attributes)
+    member = DataParse::Repo.new(attributes)
     expect(member.name).to eq('Michael Jackson')
     expect(member.html_url).to eq('5')
   end
