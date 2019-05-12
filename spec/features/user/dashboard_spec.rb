7e0f9b25f174a5e020e6d1009da863181a871a51
Blake <blake.enyart@gmail.com>
2019-05-11 18:22:15 -0600

    Add feature test for github:followers

--- spec/features/user/dashboard_spec.rb
+++ spec/features/user/dashboard_spec.rb
@@ -1,36 +1,60 @@
 require 'rails_helper'
 
-context 'As a registered user' do
+context 'As a registered user that connects Github' do
   describe 'when they visit their dashboard' do
-    it 'shows them a Github section with 5 of their repos', :vcr do
-      user = create(:user)
-      token = ENV['GITHUB_API_TOKEN']
+    describe 'and in "Github" section' do
+      it 'shows 5 of their repos', :vcr do
+        user = create(:user)
+        token = ENV['GITHUB_API_TOKEN']
 
-      allow_any_instance_of(ApplicationController).to \
-        receive(:current_user).and_return(user)
-      allow_any_instance_of(ApplicationController).to \
-        receive(:current_token).and_return(token)
+        allow_any_instance_of(ApplicationController).to \
+          receive(:current_user).and_return(user)
+        allow_any_instance_of(ApplicationController).to \
+          receive(:current_token).and_return(token)
 
-      visit dashboard_path
+        visit dashboard_path
 
-      expect(page).to have_css('.github-repos', text: 'Github')
+        expect(page).to have_css('.github', text: 'Github')
 
-      within('.github-repos') do
-        expect(page).to have_css('.github-repo', count: 5)
+        within('.github-repos') do
+          expect(page).to have_css('.github-repo', count: 5)
+        end
+      end
+
+      it 'does not show Github section if user is missing token' do
+        user = create(:user)
+
+        allow_any_instance_of(ApplicationController).to \
+          receive(:current_user).and_return(user)
+        allow_any_instance_of(ApplicationController).to \
+          receive(:current_token).and_return(nil)
+
+        visit dashboard_path
+
+        expect(page).to_not have_css('.github-repos', text: 'Github')
       end
     end
 
-    it 'does not show Github section if user is missing token' do
-      user = create(:user)
+    describe 'under "Followers" in "Github"' do
+      it 'lists all followers with their handles linking to their Github profile', :vcr do
+        user = create(:user)
+        token = ENV['GITHUB_API_TOKEN']
 
-      allow_any_instance_of(ApplicationController).to \
-        receive(:current_user).and_return(user)
-      allow_any_instance_of(ApplicationController).to \
-        receive(:current_token).and_return(nil)
+        allow_any_instance_of(ApplicationController).to \
+          receive(:current_user).and_return(user)
+        allow_any_instance_of(ApplicationController).to \
+          receive(:current_token).and_return(token)
 
-      visit dashboard_path
+        visit dashboard_path
 
-      expect(page).to_not have_css('.github-repos', text: 'Github')
+        expect(page).to have_css('.github', text: 'Github')
+
+        within('.github-followers') do
+          expect(page).to have_css('a.github-follower')
+        end
+      end
     end
   end
+
+
 end
