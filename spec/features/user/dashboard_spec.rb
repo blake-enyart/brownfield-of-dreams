8e15c28d9c1f2cd5d37b89371e385ab64c41f6b7
Blake <blake.enyart@gmail.com>
2019-05-15 17:16:48 -0600

    Update views/controllers for refactored sorted_bookmarked_videos method

--- spec/features/user/dashboard_spec.rb
+++ spec/features/user/dashboard_spec.rb
@@ -281,12 +281,20 @@ context 'As a registered user that connects Github' do
       it 'show videos organized by which tutorial they are a part of' do
         user = create(:user)
         tutorial = create(:tutorial)
-        video = tutorial.videos.create(build(:video).attributes)
+        video_1 = tutorial.videos.create(build(:video).attributes)
+        video_2 = tutorial.videos.create(build(:video).attributes)
+        video_3 = tutorial.videos.create(build(:video).attributes)
 
         allow_any_instance_of(ApplicationController).to \
           receive(:current_user).and_return(user)
 
-        visit tutorial_path(tutorial, video_id: video.id)
+        visit tutorial_path(tutorial.id, video_id: video_1.id)
+        click_on 'Bookmark'
+
+        expect(current_path).to \
+          eq(tutorial_path(tutorial))
+
+        visit tutorial_path(tutorial.id, video_id: video_2.id)
         click_on 'Bookmark'
 
         expect(current_path).to \
@@ -295,7 +303,7 @@ context 'As a registered user that connects Github' do
         visit dashboard_path
 
         within('.bookmark-videos') do
-          expect(page).to have_css('.bookmark-tutorial.bookmark-video a', count: 1)
+          expect(page).to have_css('.bookmark-tutorial .bookmark-video a', count: 1)
         end
       end
     end
