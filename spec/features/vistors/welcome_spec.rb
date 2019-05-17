daab020eb8a0419a6690c9acac1b568d183d820b
Blake <blake.enyart@gmail.com>
2019-05-16 20:14:23 -0600

    Updated test to reflect more accurately the capybara-email gem

--- spec/features/vistors/welcome_spec.rb
+++ spec/features/vistors/welcome_spec.rb
@@ -25,20 +25,22 @@ describe 'As a guest' do
   end
 
   it 'should receive email to activate account' do
-    allow_any_instance_of(ApplicationController).to \
-      receive(:current_user).and_return(@user)
+    user = User.last
 
-    expect(@user.status).to eq('Inactive')
+    allow_any_instance_of(ApplicationController).to \
+      receive(:current_user).and_return(user)
+
+    expect(user.status).to eq('inactive')
     # Will find an email sent to test@example.com
     # and set `current_email`
-    open_email(@user.email)
+    open_email(user.email)
 
     current_email.click_link 'Visit here to activate your account.'
-    @user.reload
 
     expect(current_path).to eq(activation_success_path)
     expect(page).to have_content("Thank you! Your account is now activated.")
 
+    user.reload # updates the status column
     visit dashboard_path
 
     within('.status') do
