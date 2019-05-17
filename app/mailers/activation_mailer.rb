0942edb6f30fe0c6c74e76dfa7a49eac53a792f6
Blake <blake.enyart@gmail.com>
2019-05-16 20:16:42 -0600

    Create mail action

---
+++ app/mailers/activation_mailer.rb
@@ -0,0 +1,13 @@
+class ActivationMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.activation_mailer.activation.subject
+  #
+  def activation(user)
+    @user = user
+
+    mail(to: user.email, subject: "#{user.first_name} please activate your account.")
+  end
+end
